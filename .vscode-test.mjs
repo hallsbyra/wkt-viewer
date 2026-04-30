572c0042f0dbe433e17097cef00023cf07c66147
Fredrik <fredrik@hallsbyra.se>
2026-04-30 14:52:48 +0200

    build: increase vscode test download timeout

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from '@vscode/test-cli'
 export default defineConfig({
     files: 'extension/dist/**/*.test.js',
     extensionDevelopmentPath: '.',
+    download: {
+        timeout: 60000,
+    },
     mocha: {
         timeout: 20000,
     },
